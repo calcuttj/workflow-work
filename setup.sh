342f2d19f74e98c640d3586343645b4824c2d8e6
Jacob Calcutt <calcuttj@msu.edu>
2023-12-18 15:12:52 -0600

    Fleshing out fd prod jobscript + adding requests to setup

--- setup.sh
+++ setup.sh
@@ -11,3 +11,9 @@ export MQL_REQUEST_PDSP_RAW="files from dune:all where core.file_type=detector a
 export MQL_REQUEST_PDHD_RAW="files from dune:all where core.file_type=detector and core.data_tier=raw and 22949 in core.runs limit 1"
 export MQL_REQUEST_PDSP_MC="files from dune:all where core.file_type=mc and core.data_tier=full-reconstructed and art.run_type=protodune-sp and dune.requestid=RITM1115963 and mc.space_charge=yes and 18800650 in core.runs and core.first_event_number=321"
 export MQL_REQUEST_11_22_23_DR23="files from dune:all where core.run_type=hd-protodune and core.file_type=detector and DUNE.campaign=DressRehearsalNov2023 and core.runs=23387"
+
+export MQL_REQUEST_FDPROD23_HD_NU="files from higuera:fardet-hd__fd_mc_2023a__mc__hit-reconstructed__prodgenie_nu_dune10kt_1x2x6.fcl__v09_78_01d01__preliminary limit 1"
+export MQL_REQUEST_FDPROD23_HD_ANU="files from higuera:fardet-hd__fd_mc_2023a__mc__hit-reconstructed__prodgenie_anu_dune10kt_1x2x6.fcl__v09_78_01d01__preliminary limit 1"
+
+export MQL_REQUEST_FDPROD23_VD_NU="files from higuera:fardet-vd__fd_mc_2023a__mc__hit-reconstructed__prodgenie_nu_dunevd10kt_1x8x6_3view_30deg.fcl__v09_75_03d00__preliminary limit 1"
+export MQL_REQUEST_FDPROD23_VD_ANU="files from higuera:fardet-vd__fd_mc_2023a__mc__hit-reconstructed__prodgenie_anu_dunevd10kt_1x8x6_3view_30deg.fcl__v09_75_03d00__preliminary limit 1"
