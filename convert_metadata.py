b7e4f7da1eb841fb9225cb0226f771a145523728
Jacob Calcutt <calcuttj@msu.edu>
2024-01-03 15:08:05 -0600

    adding fcl files to output metadata

--- convert_metadata.py
+++ convert_metadata.py
@@ -1,10 +1,15 @@
 from argparse import ArgumentParser as ap
+import os
 import json
+#import root_metadata
 
 
 if __name__ == '__main__':
   parser = ap()
   parser.add_argument('-i', type=str)
+  parser.add_argument('-c', type=str, default=None)
+  parser.add_argument('--det', type=str, default=None)
+  parser.add_argument('--app_ver', type=str, default=None)
   args = parser.parse_args()
 
   with open(args.i) as f:
@@ -26,7 +31,9 @@ if __name__ == '__main__':
     'group':'core.group',
   }
 
-  to_del = ['file_name', 'file_size', 'parents']
+  #to_move = [('file_size','size')]
+  to_move = []
+  to_del = ['file_name', 'parents', 'file_size']
 
   for f, newf in fields.items():
     if f not in imported_json.keys(): continue
@@ -35,6 +42,11 @@ if __name__ == '__main__':
 
   for td in to_del: del imported_json[td]
 
+  over_meta = {
+    tm[1]:imported_json[tm[0]] for tm in to_move
+  }
+  for tm in to_move: del imported_json[tm[0]]
+
   runs = imported_json['runs']
   runs_runs = [r[0] for r in runs]
   runs_subruns = [r[1] for r in runs]
@@ -42,10 +54,23 @@ if __name__ == '__main__':
   imported_json['core.runs_subruns'] = runs_subruns
   del imported_json['runs']
 
+  ##Check if various fields were reported. If so, add to metadata
+  if args.c is not None:
+    imported_json['dune.config_file'] = args.c
+
+  if args.det is not None:
+    imported_json['dune_mc.detector_type'] = args.det
+
+  if args.app_ver is not None:
+    imported_json['core.application.version'] = args.app_ver
+
   print(imported_json)
+  over_meta['metadata'] = imported_json
+  #over_meta['size'] = os.path.getsize(args.i.replace('.json', ''))
 
   # Serializing json
-  json_object = json.dumps(imported_json, indent=2)
+  #json_object = json.dumps(imported_json, indent=2)
+  json_object = json.dumps(over_meta, indent=2)
    
   # Writing to sample.json
   with open(args.i, "w") as outfile:
