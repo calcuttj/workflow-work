9ee491a391fac87572439c250147fd26c9e0c166
Jacob Calcutt <calcuttj@msu.edu>
2024-01-24 04:21:18 -0600

    Adding script to check for missing files

---
+++ check_missing_files.py
@@ -0,0 +1,32 @@
+from metacat.webapi import MetaCatClient
+from argparse import ArgumentParser as ap
+from rucio.client import Client as RucioClient
+
+if __name__ == '__main__':
+  parser = ap()
+  parser.add_argument('-q', type=str, required=True, help='Metacat query used as input to request')
+  parser.add_argument('-d', type=str, required=True, help='Rucio dataset used as output of request')
+  parser.add_argument('--type', type=str, default='hd')
+  parser.add_argument('-o', type=str, default='missing_files.txt')
+  args = parser.parse_args()
+
+  mc = MetaCatClient()
+  mc_files = [f['name'] for f in mc.query(args.q)]
+  #print(mc_files)
+
+  print(mc_files[0])
+  #stripped_mc_files = ['_'.join(f.split('_')[n1:n1+2]) for f in mc_files]
+
+  rc = RucioClient()
+  rucio_files = ['_'.join(f['name'].split('_')[:-3]) + '.root' for f in rc.list_files(*(args.d.split(':')))]
+
+  print(rucio_files[0])
+  #print('_'.join(rucio_files[0].split('_')[:-3]) + '.root')
+  #print(rucio_files)
+
+  bad_files = [i + '\n' for i in mc_files if i not in rucio_files]
+  print(bad_files)
+  print(len(bad_files))
+
+  with open(args.o, 'w') as f:
+    f.writelines(bad_files)
