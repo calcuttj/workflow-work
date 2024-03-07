59c93fd466573b65fcd35f4203cd069928926f6d
Jacob Calcutt <calcuttj@msu.edu>
2024-03-07 15:07:57 -0600

    Fleshing out metadata stuff. Adding SCE On to jobscript

--- pdhd_meta_writer.py
+++ pdhd_meta_writer.py
@@ -8,6 +8,9 @@ if __name__ == '__main__':
   parser.add_argument('--overrides', type=str, nargs='+') ##TODO -- Write help
   parser.add_argument('--parent', type=str, default=None)
   parser.add_argument('-o', type=str, required=True)
+  parser.add_argument('--run', type=int, default=None)
+  parser.add_argument('--event', type=int, default=None)
+  parser.add_argument('--nevents', type=int, required=True)
   args = parser.parse_args()
 
   if args.json is not None:
@@ -20,20 +23,34 @@ if __name__ == '__main__':
       'metadata': {}
     }
 
-  print(in_dict)
+  #print(in_dict)
 
-  for override in args.overrides:
-    split = override.split('=')
-    print(split[0], split[1])
-    in_dict['metadata'][split[0]] = split[1]
+  if args.overrides is not None:
+    for override in args.overrides:
+      split = override.split('=')
+      #print(split[0], split[1])
+      in_dict['metadata'][split[0]] = split[1]
 
-  print(in_dict)
+  #print(in_dict)
 
   if args.parent is not None:
+    name = args.parent.split(':')[0]
+    namespace = args.parent.split(':')[1]
     in_dict['parents'] = [
-      {"name": args.parent,
-       "namespace": "protodune-hd"}
+      {"name": name,
+       "namespace": namespace 
+      }
     ]
+
+  in_dict['metadata']['core.event_count'] = args.nevents
+  if args.run is not None:
+    in_dict['metadata']['core.runs'] = [args.run]
+    in_dict['metadata']['core.subruns'] = [args.run*100000 + 1]
+  if args.event is not None:
+    in_dict['metadata']['core.first_event_number'] = args.event
+    in_dict['metadata']['core.last_event_number'] = (
+        args.event + args.nevents - 1)
+
 
 
   # Serializing json
